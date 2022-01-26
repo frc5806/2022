6b75ef5a359d88f094e412447f7340c8ac1962ce
benjaminmay <43893499+benjaminmay@users.noreply.github.com>
2022-01-25 20:02:45 -0500

    Update driveForTime.java

--- 2022/src/main/java/frc/robot/driveForTime.java
+++ 2022/src/main/java/frc/robot/driveForTime.java
@@ -11,16 +11,17 @@ boolean completed
 
 boolean wasInMotion
 
-public driveForTime(double start, double end, boolean s){
+public driveForTime(double start, double end){
     completed=false;
     startMotion = start
     endMotion = end;
-    succesion = s;
+    succesion = false;
     wasInMotion = false;
 
 }
 
-public void run(double speed, double turn){
+public void run(double speed, double turn, boolean s){
+    succesion = s;
     if(Robot.time < endMotion && Robot.time > startMotion && succesion){
         wasInMotion = true;
         Robot.drive.drive(speed, turn);
