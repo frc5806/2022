9c2e95312839dd7c541553052ce4c3402e5a992e
Ben-coder1 <43893499+Ben-coder1@users.noreply.github.com>
2022-04-21 20:05:11 -0400

    Update Shooter.java

--- 2022/src/main/java/frc/robot/Shooter.java
+++ 2022/src/main/java/frc/robot/Shooter.java
@@ -15,6 +15,13 @@ public class Shooter {
     static public CANSparkMax shooter2;
     static public VictorSPX shooter3;
 
+    private final SparkMaxPIDController pid1;
+    private final SparkMaxPIDController pid2;
+    private double velocity;
+
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+    public double kP2, kI2, kD2, kIz2, kFF2, kMaxOutput2, kMinOutput2;
+
     static public double speed;
     private double targetRPM;
 
@@ -23,8 +30,39 @@ public class Shooter {
         shooter1 = new CANSparkMax(CANID1, MotorType.kBrushless);
         shooter2 = new CANSparkMax(CANID2, MotorType.kBrushless);
         shooter3 = new VictorSPX(CANID3);
+        pid1 = shooter1.getPIDController();
+        pid2 = shooter2.getPIDController();
+        
         constants = new Constants();
         speed = 0;
+        kP = 0.014872; 
+        kI = 1e-4;
+        kD = 0; 
+        kIz = 0; 
+        kFF = 0; 
+        kMaxOutput = 1; 
+        kMinOutput = -1;
+  
+        pid2.setP(kP);
+        pid2.setI(kI);
+        pid2.setD(kD);
+        pid2.setIZone(kIz);
+        pid2.setFF(kFF);
+        pid2.setOutputRange(kMinOutput, kMaxOutput);
+        kP2 =  0.014872; 
+        kI2 = 1e-4;
+        kD2 = 0; 
+        kIz2 = 0; 
+        kFF2 = 0; 
+        kMaxOutput2 = 1; 
+        kMinOutput2 = -1;
+    // set PID coefficients
+        pid1.setP(kP);
+        pid1.setI(kI);
+        pid1.setD(kD);
+        pid1.setIZone(kIz);
+        pid1.setFF(kFF);
+        pid1.setOutputRange(kMinOutput, kMaxOutput);
         
         
     }
@@ -52,6 +90,17 @@ public class Shooter {
         }
     }
 
+    public void setSpeedPID(double vel){
+        velocity=vel;
+    }
+
+    public void update(){
+        pid1.setReference(velocity, CANSparkMax.ControlType.kVelocity);
+        pid2.setReference(velocity, CANSparkMax.ControlType.kVelocity);
+        shooter3.set(ControlMode.PercentOutput, velocity/5800);
+    }
+
+
     public void dontShoot() {
         shooter1.set(0);
         shooter2.set(0);
@@ -69,7 +118,7 @@ public class Shooter {
         // set constant speed if more than 30 inches
             targetRPM = 3750;
 
-        shoot(targetRPM/Constants.maxRPM);
+        setSpeedPID(targetRPM);
     }
 
 }
